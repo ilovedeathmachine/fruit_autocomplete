724b6bb6304c172942e5d1879a3206fa379ac377
Gabrielle <124479113+ilovedeathmachine@users.noreply.github.com>
2023-08-01 18:33:28 -0400

    Add files via upload

---
+++ new.js
@@ -0,0 +1,20 @@
+const input = document.querySelector('#input');
+const suggestions = document.querySelector('.suggestions ul');
+const matchList = document.querySelector('#matchList');
+
+const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+input.addEventListener("input", () => {
+	const inputVal = input.value.toLowerCase();
+	const matches = fruits.filter(fruit => fruit.toLowerCase().includes(inputVal));
+	showSuggestions(matches);
+});
+
+function showSuggestions(matches) {
+	if (matches.length > 0) {
+		const html = matches.map(match => `<li>${match}</li>`).join("");
+		matchList.innerHTML = html;
+	} else {
+		matchList.innerHTML = "";
+	}
+}
